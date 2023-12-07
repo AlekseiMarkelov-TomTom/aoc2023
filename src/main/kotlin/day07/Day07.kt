61040821203665cde61f512d668c6464f826bfc7
Aleksei Markelov <aleksei.markelov@tomtom.com>
2023-12-07 20:10:36 +0100

    day07 puzzle2

--- src/main/kotlin/day07/Day07.kt
+++ src/main/kotlin/day07/Day07.kt
@@ -19,6 +19,21 @@ val cardValues = mapOf(
     '2' to 2
 )
 
+val cardValuesWithJoker = mapOf(
+    'A' to 14,
+    'K' to 13,
+    'Q' to 12,
+    'T' to 10,
+    '9' to 9,
+    '8' to 8,
+    '7' to 7,
+    '6' to 6,
+    '5' to 5,
+    '4' to 4,
+    '3' to 3,
+    '2' to 2,
+    'J' to 1,
+)
 enum class HandType(val value: Int) {
     FiveOfAKind(6), FourOfAKind(5), FullHouse(4), ThreeOfAKind(3), TwoPairs(2), OnePair(1), HighCard(0)
 
@@ -26,6 +41,8 @@ enum class HandType(val value: Int) {
 
 // Conversion function from Char to Card
 fun cardValue(card: Char) = cardValues[card]!!
+
+fun cardValueWithJoker(card: Char) = cardValuesWithJoker[card]!!
 
 
 data class Hand(val cards: String, val bid: Long) : Comparable<Hand> {
@@ -56,6 +73,41 @@ data class Hand(val cards: String, val bid: Long) : Comparable<Hand> {
     }
 }
 
+data class HandWithJoker(val cards: String, val bid: Long) : Comparable<HandWithJoker> {
+    fun type(): HandType {
+        val counts = cards.groupingBy { it }.eachCount()
+        val jokersCount = counts.getOrDefault('J', 0)
+        if (jokersCount == 5) {
+            return HandType.FiveOfAKind
+        }
+        val countsWithoutJoker = counts.filterKeys { it != 'J' }.values.sortedDescending().toMutableList()
+        countsWithoutJoker[0] += jokersCount
+        return when (countsWithoutJoker) {
+            listOf(5) -> HandType.FiveOfAKind
+            listOf(4, 1) -> HandType.FourOfAKind
+            listOf(3, 2) -> HandType.FullHouse
+            listOf(3, 1, 1) -> HandType.ThreeOfAKind
+            listOf(2, 2, 1) -> HandType.TwoPairs
+            listOf(2, 1, 1, 1) -> HandType.OnePair
+            else -> HandType.HighCard
+        }
+    }
+
+    override fun compareTo(other: HandWithJoker): Int {
+        val typeComparison = type().value.compareTo(other.type().value)
+        if (typeComparison != 0) {
+            return typeComparison
+        }
+        for ((card1, card2) in cards.zip(other.cards)) {
+            val comparison = cardValueWithJoker(card1).compareTo(cardValueWithJoker(card2))
+            if (comparison != 0) {
+                return comparison
+            }
+        }
+        return 0
+    }
+}
+
 fun parseInput(input: List<String>): List<Hand> {
     return input.asSequence().map { val (cards, bidString) = it.split(' '); Hand(cards, bidString.toLong()) }.toList()
 }
@@ -69,7 +121,12 @@ fun part1(input: List<String>): Long {
 }
 
 fun part2(input: List<String>): Long {
-    return input.size.toLong()
+    val sorted = input.asSequence().map { val (cards, bidString) = it.split(' '); HandWithJoker(cards, bidString.toLong()) }.toList().sorted()
+
+    for (hand in sorted) {
+        println("%s %d - %s".format(hand.cards, hand.bid, hand.type().name))
+    }
+    return sorted.asSequence().mapIndexed{ index, hand -> (index + 1) * hand.bid }.sum()
 }
 
 fun main() {
