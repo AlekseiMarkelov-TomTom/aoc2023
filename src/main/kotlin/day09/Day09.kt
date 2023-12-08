9ee3bd2b85b342bbbd77bdfba2a7196bc67b8843
Aleksei Markelov <aleksei.markelov@tomtom.com>
2023-12-09 00:16:35 +0100

    day7 part2 expectations

---
+++ src/main/kotlin/day09/Day09.kt
@@ -0,0 +1,19 @@
+package day09
+
+import println
+import readInput
+
+
+fun part1(input: List<String>): Long {
+    return input.size.toLong()
+}
+
+fun part2(input: List<String>): Long {
+    return input.size.toLong()
+}
+
+fun main() {
+    val input = readInput("Day09")
+    part1(input).println()
+    part2(input).println()
+}
--- src/test/kotlin/Day09Test.kt
+++ src/test/kotlin/Day09Test.kt
@@ -1,17 +1,16 @@
 package test.kotlin
 
-import day07.part1
-import day07.part2
+import day09.part1
+import day09.part2
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 import readInput
-import java.nio.channels.AsynchronousServerSocketChannel
 
-class Day07Test {
+class Day09Test {
     @ParameterizedTest
     @CsvSource(
-        "Day07_test, 6440", "Day07, 250453939"
+        "Day09_test, 0", "Day09, 0"
     )
     fun testPart1(inputFileName: String, expectedResult: Long) {
         Assertions.assertEquals(expectedResult, part1(readInput(inputFileName)))
@@ -19,17 +18,9 @@ class Day07Test {
 
     @ParameterizedTest
     @CsvSource(
-        "Day07_test, 5905", "Day07, 248652697"
+        "Day09_test, 0", "Day09, 0"
     )
     fun testPart2(inputFileName: String, expectedResult: Long) {
         Assertions.assertEquals(expectedResult, part2(readInput(inputFileName)))
-    }
-
-    @ParameterizedTest
-    @CsvSource(
-        "AAT4J, OnePair"
-    )
-    fun testTyping(input: String, expectedType: String) {
-        Assertions.assertEquals(expectedType, day07.Hand(input, 1).type().name)
     }
 }
